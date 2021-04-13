78da08c54511a130adcafd763745829d97ef634b
jorgerubira <53122255+jorgerubira@users.noreply.github.com>
2021-04-13 20:52:19 +0200

    Update Form1.Designer.cs

--- Prueba1/Form1.Designer.cs
+++ Prueba1/Form1.Designer.cs
@@ -57,7 +57,7 @@
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(12, 77);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.Size = new System.Drawing.Size(50, 13);
             this.label1.TabIndex = 2;
             this.label1.Text = "label1";
             // 
