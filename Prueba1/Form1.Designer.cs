13c63ca3d624aaa8ec8125db9f80a2b246951872
jorgerubira <53122255+jorgerubira@users.noreply.github.com>
2021-04-13 20:54:02 +0200

    Update Form1.Designer.cs

--- Prueba1/Form1.Designer.cs
+++ Prueba1/Form1.Designer.cs
@@ -40,7 +40,7 @@
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "eyeryeryery";
             this.button1.UseVisualStyleBackColor = true;
             // 
             // button2
@@ -49,7 +49,7 @@
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 1;
-            this.button2.Text = "button2";
+            this.button2.Text = "yryr";
             this.button2.UseVisualStyleBackColor = true;
             // 
             // label1
